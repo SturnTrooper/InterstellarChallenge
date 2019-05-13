9cb3c5af05ce12eed028040fbf9544411cc3ad01
SturnTrooper <sturntrooper@gmail.com>
2019-05-13 23:32:34 +0200

    added class description

--- src/de/interstellar/model/data/JSONDataProvider.java
+++ src/de/interstellar/model/data/JSONDataProvider.java
@@ -1,5 +1,14 @@
 package de.interstellar.model.data;
 
+/** This class provides the functionality to obtain data that is formatted in the
+ *  JavaScript Object Notation (JSON) file format. 
+ *  
+ *  The data can be received from an URL or the file system.
+ *  
+ * @author Florian Sturn
+ * @date 14.05.2019
+ *
+ */
 public class JSONDataProvider {
 
 }
